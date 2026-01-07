088a50bb4e35568595ac7ec2cfef40d086861719
Devansha-Awasthi <devansha.awasthi@example.com>
2026-01-07 13:26:22 +0530

    Added multiplication feature

--- calculator.py
+++ calculator.py
@@ -4,6 +4,10 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
+
 if __name__ == "__main__":
     print("Simple Calculator")
     x = int(input("Enter first number: "))
@@ -11,3 +15,6 @@ if __name__ == "__main__":
 
     print("Addition:", add(x, y))
     print("Subtraction:", subtract(x, y))
+    print("Multiplication:", multiply(x, y))
+
+
